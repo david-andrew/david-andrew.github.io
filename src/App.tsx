f52ee5184a907f0b80e0aa1a96f7ff0bede42947
David Samson <david.andrew.engineer@gmail.com>
2021-05-13 20:56:31 -0400

    set up prettier + semantic-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
-import React from 'react';
-import { HomePage } from './Pages/Home';
+import React from 'react'
+import { Navbar } from './Components/Navbar'
+import { HomePage } from './Pages/Home'
 
 function App() {
     return (
-        <div style={{backgroundColor: 'black', width:'100vw', height:'100vh', color:'white'}}>
-            <HomePage/>
+        <div
+            style={{
+                backgroundColor: 'grey',
+                width: '100vw',
+                height: '100vh',
+                color: 'white',
+            }}
+        >
+            <Navbar />
+            <HomePage />
         </div>
-    );
+    )
 }
 
-export default App;
+export default App
---
+++ src/Components/Navbar.tsx
@@ -0,0 +1,20 @@
+import React, {useState} from 'react'
+import {Menu, MenuItemProps} from 'semantic-ui-react'
+
+export const Navbar = (): JSX.Element => {
+    const [activeItem, setActiveItem] = useState<string | undefined>(undefined);
+    
+    const handleOnClick = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>, {name}: MenuItemProps) => {setActiveItem(name)}
+    
+    return (
+        <div style={{width:'100vw', backgroundColor: 'blue', textAlign: 'center'}}>
+            <Menu secondary>
+                <Menu.Item name='Home' active={activeItem === 'Home'} onClick={handleOnClick}/>
+                <Menu.Item name='Projects' active={activeItem === 'Projects'} onClick={handleOnClick}/>
+                <Menu.Item name='About' active={activeItem === 'About'} onClick={handleOnClick}/>
+                <Menu.Item name='Misc.' active={activeItem === 'Misc.'} onClick={handleOnClick}/>
+                <Menu.Item name='Contact' active={activeItem === 'Contacts'} onClick={handleOnClick}/>
+            </Menu>
+        </div>
+    )
+}
