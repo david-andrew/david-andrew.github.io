3873f553623eaeb822f3601ee74e8b30fc4b2fdf
David Samson <david.andrew.engineer@gmail.com>
2021-05-30 19:21:26 -0400

    started writing foxing article

--- src/Pages/Projects/FoxingAnimatronic.tsx
+++ src/Pages/Projects/FoxingAnimatronic.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import { PageContainer } from '../../Components'
+import { PageContainer, PageHeading } from '../../Components'
 
 export const FoxingAnimatronic = (): JSX.Element => {
     return (
         <>
             <PageContainer>
-                <h1>Foxing Animatronic</h1>
+                <PageHeading title="Foxing Animatronic" subtitile="June 2018" />
             </PageContainer>
         </>
     )
