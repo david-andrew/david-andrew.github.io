264ed2fc1667513ba8fad85470271751b66cdf5c
David A. Samson <david.andrew.engineer@gmail.com>
2021-06-06 11:17:25 -0400

    making dewy wrapper code more correct

--- src/wasm/dewy_parser_wrapper.c
+++ src/wasm/dewy_parser_wrapper.c
@@ -1,49 +1,66 @@
-#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
 #include <string.h>
 
-#include "ustring.h"
 #include "utilities.h"
+#include "object.h"
+#include "ustring.h"
+#include "metatoken.h"
+#include "metascanner.h"
+#include "metaparser.h"
+#include "metaitem.h"
+#include "srnglr.h"
+#include "dewy_parser_wrapper.h"
 
-size_t cstring_to_ustring(char *src, uint32_t **dest);
-
-void dewy_parser(char *grammar_source, char *input_utf8)
+//run the parser wrapper with example inputs
+//TODO->remove this when everythings working
+int main()
 {
-    //convert the input source to a uint32_t string
-    uint32_t *input_source;
-    size_t input_size = cstring_to_ustring(input_utf8, &input_source);
-
-    //set up structures for the sequence of scanning/parsing
-    initialize_metascanner();
-    initialize_metaparser();
-    initialize_srnglr(input_size);
-
-    if (!run_compiler_compiler(grammar_source)) //, verbose, scanner, ast, parser, grammar, table))
-    {
-        goto cleanup;
-    }
-    if (!run_compiler(input_source)) //, compile, forest))
-    {
-        goto cleanup;
-    }
-
-cleanup:
-    free(grammar_source);
-    free(input_source);
-
-    release_metascanner();
-    release_metaparser();
-    release_srnglr();
-
+    char *grammar_source = "#E = '(' #ws * #E #ws * ')';\n"
+                           "#E = #E #ws *[+\\-] #ws * #E;\n"
+                           "#E = #E #ws *[*/] #ws * #E;\n"
+                           "#E = #E #ws * '^' #ws * #E;\n"
+                           "#E = #N | #I;\n"
+                           "#N = [0-9]+;\n"
+                           "#I = [A-Za-z_][A-Za-z0-9!@%%&_?]*;\n"
+                           "#ws = [\n\x20];\n"
+                           "#start = (#ws* #E)+;";
+    char *input_source = "1+2*3";
+    dewy_parser(grammar_source, input_source);
     return 0;
 }
 
-size_t cstring_to_ustring(char *src, uint32_t **dest)
+void dewy_parser(char *grammar_source, char *input_utf8)
 {
-    //compute number of unicode characters in string
-    size_t length = utf8_length(src);
+    //convert the input source to a uint32_t string, and get its length
+    size_t input_utf8_size = utf8_length(input_utf8);
+    uint32_t *input_source = ustring_charstar_substr(input_utf8, 0, input_utf8_size - 1);
 
-    //get the unicode version of the string by taking a unicode substring of the whole length
-    *dest = ustring_charstar_substr(src, 0, length - 1);
+    //DEBUG just print out the grammar and input
+    printf("grammar:\n%s\n\nsource:\n", grammar_source);
+    ustring_str(input_source);
+    printf("\n");
 
-    return length;
+    //     //set up structures for the sequence of scanning/parsing
+    //     initialize_metascanner();
+    //     initialize_metaparser();
+    //     initialize_srnglr(input_utf8_size);
+
+    //     if (!run_compiler_compiler(grammar_source)) //, verbose, scanner, ast, parser, grammar, table))
+    //     {
+    //         goto cleanup;
+    //     }
+    //     if (!run_compiler(input_source)) //, compile, forest))
+    //     {
+    //         goto cleanup;
+    //     }
+
+    // cleanup:
+    //     free(grammar_source);
+    //     free(input_source);
+
+    //     release_metascanner();
+    //     release_metaparser();
+    //     release_srnglr();
 }
---
+++ src/wasm/dewy_parser_wrapper.h
@@ -0,0 +1,21 @@
+#ifndef DEWY_PARSER_WRAPPER
+#define DEWY_PARSER_WRAPPER
+
+#include <stdint.h>
+
+#include "vector.h"
+#include "metaast.h"
+#include "set.h"
+
+void dewy_parser(char *grammar_source, char *input_utf8);
+
+bool run_compiler_compiler(char *source, bool verbose, bool scanner, bool ast, bool parser, bool grammar, bool table);
+bool run_compiler(uint32_t *source, bool compile, bool forest);
+void print_scanner(vect *tokens, bool verbose);
+void print_ast(uint64_t head_idx, metaast *body_ast, bool verbose);
+void print_parser(bool verbose);
+void print_grammar();
+void print_table();
+void print_compiler();
+
+#endif
