3973b6129273d066d7ecf563291e0421f6508326
David Samson <david.andrew.engineer@gmail.com>
2021-06-16 23:37:43 -0400

    started writing musical dl article

--- src/Pages/Projects/MusicalDL.tsx
+++ src/Pages/Projects/MusicalDL.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
-import { PageContainer } from '../../Components'
+import { PageContainer, PageHeading } from '../../Components'
+import { useGithubTimestamp } from '../../utilities'
 
 export const MusicalDL = (): JSX.Element => {
+    const timestamp = useGithubTimestamp('MusicalDL')
+
     return (
         <>
             <PageContainer>
-                <h1>Musical Deep Learning</h1>
+                <PageHeading title="Musical Deep Learning" subtitle={timestamp} />
+                <p>
+                    For the capstone project of Machine Learning: Deep Learning (EN.601.682), we were tasked to form teams and apply deep learning to a topic of
+                    our choosing. My team of 3 was interested in music, so we attempted to create a deep learning based synthesizer, targeted at choral music.
+                </p>
             </PageContainer>
         </>
     )
