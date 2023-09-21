c96e12fb2575145bf4d420611593adf9a5f72625
David Samson <david.andrew.engineer@gmail.com>
2023-09-20 23:15:10 -0400

    added some skeleton logic for page numbers on projects list page

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,7 +1,21 @@
+"use client"
+import { useSearchParams } from "next/navigation"
+
 export default function Test() {
+    const maxPage = 10; //TODO: get this from the projects pages
+
+    const params = useSearchParams();
+
+    // get the current page number from the url (or default to 1)
+    let page: number = parseInt(params.get("page") ?? '');
+    if (isNaN(page)) page = 1;
+    if (page < 1) page = 1;
+    if (page > maxPage) page = maxPage;
+    
     return (
         <div>
             <h1>Projects Page</h1>
+            <h2>Page: {page}</h2>
         </div>
     )
 }
