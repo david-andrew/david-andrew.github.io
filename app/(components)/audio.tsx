a173031c758022911d77af6ff40a9770d7bb5a47
David Samson <david.andrew.engineer@gmail.com>
2023-10-23 23:55:07 -0400

    added rough audio grid component and filled in files for so_voice page

--- app/(components)/audio.tsx
+++ app/(components)/audio.tsx
@@ -1,10 +1,32 @@
 "use client";
 import ReactAudioPlayer from "react-audio-player";
+import { Divider } from "./ui";
 
-export const ClipCollage = (): JSX.Element => {
+type AudioGridProps = {
+    title: string;
+    clips: {
+        label: string;
+        src: string;
+    }[];
+}
+
+export const AudioGrid = ({ title, clips }: AudioGridProps) => {
     return (
-        <>
-            <ReactAudioPlayer src="/audio/so_voice/atma16000.wav" controls/>
-        </>
-    )
+        <div className="bg-gray-800 text-white p-6 rounded-md">
+            <h1 className="text-2xl mb-2 font-quadon">{title}</h1>
+            <Divider/>
+            <div className="grid grid-cols-2 gap-6">
+                {clips.map(({ label: name, src }, index) => (
+                    <div key={index} className="flex flex-col items-start">
+                        <p className="mb-2 text-xl font-quadon">{name}</p>
+                        <ReactAudioPlayer
+                            src={src}
+                            controls
+                            className="w-full"
+                        />
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 }
--- app/projects/so_voice/page.tsx
+++ app/projects/so_voice/page.tsx
@@ -1,9 +1,24 @@
-import { ClipCollage } from "@/app/(components)/audio";
+import { AudioGrid } from "@/app/(components)/audio";
 
 const Page = (): JSX.Element => {
     return (
         <>
-            <ClipCollage/>
+            <AudioGrid
+                title="Audio Resynthesis (16000 Hz)"
+                clips={[
+                    {label: 'Mother and Child (Real)', src: '/audio/so_voice/atma16000.wav'},
+                    {label: 'Mother and Child (Resynthesized)', src: '/audio/so_voice/atma16000resynth.wav'},
+                    
+                    {label: 'When David Heard (1) (Real)', src: '/audio/so_voice/david1.wav'},
+                    {label: 'When David Heard (1) (Resynthesized)', src: '/audio/so_voice/david1_hat.wav'},
+                    
+                    {label: 'When David Heard (2) (Real)', src: '/audio/so_voice/david2.wav'},
+                    {label: 'When David Heard (2) (Resynthesized)', src: '/audio/so_voice/david2_hat.wav'},
+                    
+                    {label: 'Nox Arumque (Real)', src: '/audio/so_voice/nox.wav'},
+                    {label: 'Nox Arumque (Resynthesized)', src: '/audio/so_voice/nox_hat.wav'},
+                ]}
+            />
         </>
     )
 }
