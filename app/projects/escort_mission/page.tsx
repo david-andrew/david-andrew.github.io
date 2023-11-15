d1744d7f491f22746ecc6c51b3c79574e6c8e242
David Samson <david.andrew.engineer@gmail.com>
2023-11-14 21:59:06 -0500

    added escort mission page

--- app/projects/escort_mission/page.tsx
+++ app/projects/escort_mission/page.tsx
@@ -1,5 +1,13 @@
-const Page = (): JSX.Element => {
-    return <h1>Escort Mission 2020</h1>
+import { Content } from './content'
+import { getImages } from '@/app/projects/fetch'
+
+
+const Page = async (): Promise<JSX.Element> => {
+    const images = await getImages('escort_mission')
+
+    return (
+        <Content images={images} />
+    )
 }
 
 export default Page;
