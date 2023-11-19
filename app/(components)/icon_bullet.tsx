c17469b146ea78bc0bcb46ba327fb57967d90e5b
David Samson <david.andrew.engineer@gmail.com>
2023-11-18 21:50:29 -0500

    added wse18 page

--- app/(components)/icon_bullet.tsx
+++ app/(components)/icon_bullet.tsx
@@ -9,6 +9,7 @@ import hashtag from "@/app/(images)/icons/hashtag.svg";
 import object_group from "@/app/(images)/icons/object_group.svg";
 import jhu_hub from "@/app/(images)/icons/jhu_hub.svg";
 import chrome from "@/app/(images)/icons/chrome.svg";
+import table from "@/app/(images)/icons/table.svg";
 
 
 //TODO: is there a way to specify the type of the object keys?
@@ -24,6 +25,7 @@ const icon_map = {
     "object group": object_group,
     "jhu hub": jhu_hub,
     chrome,
+    table,
 } as const;
 type Icon = keyof typeof icon_map;
 
