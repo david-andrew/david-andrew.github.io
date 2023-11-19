d6e7a3f88a5c60884bcdf5387743bbfacf91981c
David Samson <david.andrew.engineer@gmail.com>
2023-11-18 22:33:54 -0500

    added simple page for this website

--- app/projects/website/page.tsx
+++ app/projects/website/page.tsx
@@ -1,5 +1,19 @@
+import { P, Link } from "@/app/(components)/ui";
+import { IconBullet, IconBulletList } from "@/app/(components)/icon_bullet";
+
 const Page = (): JSX.Element => {
-    return <h1>This Website</h1>
+    return (
+        <>
+            <P>
+                This website was made using <Link href="https://nextjs.org">Next.js</Link> and <Link href="https://tailwindcss.com">Tailwind CSS</Link>.
+            </P>
+            <IconBulletList>
+                <IconBullet icon="github">
+                    <Link href="https://github.com/david-andrew/david-andrew.github.io">github repo</Link>
+                </IconBullet>
+            </IconBulletList>
+        </>
+    )
 }
 
 export default Page;
