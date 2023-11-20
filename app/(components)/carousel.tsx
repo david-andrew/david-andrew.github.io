6eff8f128b804967e72960c8ea5f928400620e2d
David Samson <david.andrew.engineer@gmail.com>
2023-11-19 21:33:32 -0500

    fixed another compiler warning relating to carousel

--- app/(components)/carousel.tsx
+++ app/(components)/carousel.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Image, { StaticImageData } from "next/image";
-import { useEffect } from "react";
+import { useCallback, useEffect } from "react";
 import { ChevronLeftIcon, ChevronRightIcon, XMarkIcon } from '@heroicons/react/24/outline';
 import { useSwipeable } from 'react-swipeable';
 
@@ -24,18 +24,24 @@ export const Carousel = ({images, i, isOpen, setIsOpen, setIdx, loop=false}:Caro
     }, [isOpen]);
 
     // handling next and prev
-    const handleNext = () => {
-        if (i === images.length - 1) 
-            setIdx(loop ? 0 : i);
-        else 
-            setIdx(i + 1);
-    }
-    const handlePrev = () => {
-        if (i === 0) 
-            setIdx(loop ? images.length - 1 : i);
-        else 
-            setIdx(i - 1);
-    }
+    const handleNext = useCallback( 
+        () => {
+            if (i === images.length - 1) 
+                setIdx(loop ? 0 : i);
+            else 
+                setIdx(i + 1);
+        },
+        [i, images.length, loop, setIdx]
+    );
+    const handlePrev = useCallback(
+        () => {
+            if (i === 0) 
+                setIdx(loop ? images.length - 1 : i);
+            else 
+                setIdx(i - 1);
+        },
+        [i, images.length, loop, setIdx]
+    );
 
 
     //close the carousel when the user presses escape. Also handle next and prev with arrow keys
