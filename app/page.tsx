b44a6a495f98c3f6aedc8c42215b01040965c0fc
David Samson <david.andrew.engineer@gmail.com>
2023-12-15 21:26:12 -0500

    fixed loading timestamps so that it doesn't matter what page you are on for it to run

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,10 @@
 import Image from 'next/image'
 import profile from '@/app/(images)/profile.jpg'
 import { NavbarDummy } from '@/app/(components)/navbar'
-import { GithubTimestampsFetcher } from '@/app/projects/context'
-import { getProjects } from '@/app/projects/fetch'
 
-
-const Home = async (): Promise<JSX.Element> => {
-    const projects = await getProjects();
-
+const Home = (): JSX.Element => {
     return (
         <>
-            <GithubTimestampsFetcher projects={projects}/>
             <div className="flex flex-col pointer-events-none select-none justify-center items-center font-quadon text-center h-full">
                 <div style={{fontSize:'10vmin'}}>David-Andrew Samson</div>
                 <Image src={profile} alt='Image of David' style={{maxWidth:'50vmin'}} />
