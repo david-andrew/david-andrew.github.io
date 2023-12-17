51f197c94f8cdd83461b3b6728ee941c8c57866c
David Samson <david.andrew.engineer@gmail.com>
2023-12-17 15:50:21 -0500

    switched order of keywords on title page

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ const Home = (): JSX.Element => {
             <div className="flex flex-col pointer-events-none select-none justify-center items-center font-quadon text-center h-full">
                 <div style={{fontSize:'10vmin'}}>David-Andrew Samson</div>
                 <Image src={profile} alt='Image of David' style={{maxWidth:'50vmin'}} />
-                <div style={{fontSize:'5vmin'}}>AI/ML - Music - Engineering</div>
+                <div style={{fontSize:'5vmin'}}>AI/ML - Engineering - Music</div>
                 <NavbarDummy />
             </div>
         </>
