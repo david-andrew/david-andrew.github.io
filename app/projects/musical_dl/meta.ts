9e6addd24770b6bfb1ee1a7511d91f313af32179
David Samson <david.andrew.engineer@gmail.com>
2023-12-17 16:34:41 -0500

    made musicaldl use github repo for date, rather than hardcoded value

--- app/projects/musical_dl/meta.ts
+++ app/projects/musical_dl/meta.ts
@@ -4,7 +4,7 @@ import logo from '@/app/(images)/logo.png'
 export const meta: ProjectMeta = {
     title: 'Musical DL',
     imgSrc: logo,
-    lastUpdated: 'June 2022',
+    github:'MusicalDL',
     summary: 'Using deep learning to generate choral music in the style of JS Bach',
     tags: ['Python', 'Pytorch', 'AI/ML', 'choral', 'music', 'generation'],
 };
