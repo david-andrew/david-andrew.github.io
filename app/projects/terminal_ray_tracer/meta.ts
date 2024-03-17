fed3b62c1d31b3ff950112beb1a4b526417ffd22
David Samson <david.andrew.engineer@gmail.com>
2024-03-17 19:30:14 -0400

    added tags for terminal ray tracer

--- app/projects/terminal_ray_tracer/meta.ts
+++ app/projects/terminal_ray_tracer/meta.ts
@@ -6,4 +6,5 @@ export const meta: ProjectMeta = {
     imgSrc: terminal_ray_tracer,
     github: 'TerminalRayTracer',
     summary: 'A dependency-free ray tracer written in C that runs directly in a linux terminal',
+    tags: ['C', 'ray tracing', 'CLI', 'linux'],
 }
