42bb64cb27ff6d907f1cede3e8acd8ec90dde3b7
David Samson <david.andrew.engineer@gmail.com>
2024-03-17 20:10:50 -0400

    added soundtrack and download links and adjusted link locations

--- app/projects/silver_void/page.tsx
+++ app/projects/silver_void/page.tsx
@@ -2,6 +2,7 @@ import { getImages } from '@/app/projects/fetch'
 import { Link, P, H3, UL } from '@/app/(components)/ui'
 import { IconBullet, IconBulletList } from '@/app/(components)/icon_bullet'
 import { Collage } from '@/app/(components)/collage'
+import { YouTube } from '@/app/(components)/youtube'
 
 const Page = async (): Promise<JSX.Element> => {
     const images = await getImages('silver_void')
@@ -10,14 +11,20 @@ const Page = async (): Promise<JSX.Element> => {
         <>
             {/* TODO: put html5 demo here */}
             <P>
-                Silver Void was my submission to <Link href="https://itch.io/jam/acerola-jam-0">Acerola Jam 0</Link>.
-                Participants had 2 weeks to solo create a game on the theme &quot;Aberration&quot;, as in a departure
-                from what is normal, usual, or expected. Initially I wanted to make a time loop game, sort of like
-                Groundhog Day or Re:Zero, but quickly determined that even with 2 weeks, the scope was too large. So I
-                settled on making a realistic 3D space simulator, where the player takes part in a fleet battle. This
-                fits the theme in that the enemies continually predict where the player will be based on current
-                position and velocity, and so the player needs to continually thrust and change trajectories (i.e. make
-                an aberration from their current path) to avoid being hit.
+                <Link className="text-3xl font-quadon" href="https://dsamson.itch.io/silver-void">
+                    Play/Download on Itch.io
+                </Link>
+            </P>
+            <P>
+                Silver Void was my <Link href="https://itch.io/jam/acerola-jam-0/rate/2557875">submission</Link> to{' '}
+                <Link href="https://itch.io/jam/acerola-jam-0">Acerola Jam 0</Link>. Participants had 2 weeks to solo
+                create a game on the theme &quot;Aberration&quot;, as in a departure from what is normal, usual, or
+                expected. Initially I wanted to make a time loop game, sort of like Groundhog Day or Re:Zero, but
+                quickly determined that even with 2 weeks, the scope was too large. So I settled on making a realistic
+                3D space simulator, where the player takes part in a fleet battle. This fits the theme in that the
+                enemies continually predict where the player will be based on current position and velocity, and so the
+                player needs to continually thrust and change trajectories (i.e. make an aberration from their current
+                path) to avoid being hit.
             </P>
             <P>
                 Usually I approach game making as an art form, and for this jam, I feel I was really channelling The
@@ -28,15 +35,21 @@ const Page = async (): Promise<JSX.Element> => {
             </P>
             <H3>Photos</H3>
             <Collage images={images} rowSizes={[2, 3]} />
-            <H3>Technical</H3>
+            <H3>Assets</H3>
             <P>
                 All assets except the milky way image (public domain here:{' '}
                 <Link href="https://commons.wikimedia.org/wiki/File:ESO_-_Milky_Way.jpg">ESO - Mikly Way</Link>) were
                 made by me. Sound effects are pot and pan recordings layered/edited in audacity, and the soundtrack is
                 piano improv recorded with my phone. There&apos;s not much art to speak of, as ships, bullets, and
                 explosions are all just simple polygons combined into slightly more complex shapes. The game itself was
-                made in the Godot game engine
+                made in the Godot game engine.
             </P>
+            <H3>Soundtrack</H3>
+            <P>
+                I didn&apos;t have a lot of time to work on the soundtrack, so it&apos;s just a single take. But
+                apparently people enjoyed it a lot, so here it is:
+            </P>
+            <YouTube videoId="0t_NKVzPSF8" />
             <H3>Cut Features</H3>
             <P>
                 Lots of things had to be cut. Initially when I was building the ships, I was imagining fleet battles
@@ -67,39 +80,32 @@ const Page = async (): Promise<JSX.Element> => {
                 </li>
                 <li>better fleet behavior</li>
             </UL>
-            {/* <H3>Try It</H3> */}
-            {/* <IconBulletList>
+            <H3>Try It</H3>
+            <IconBulletList>
                 <IconBullet icon="chrome">
-                    <Link href="https://david-andrew.github.io/silver_void_webgl/">Silver Void (web)</Link>
+                    <Link href="https://dsamson.itch.io/silver-void">Web Version</Link>
                 </IconBullet>
                 <IconBullet icon="linux">
-                    <Link href="https://github.com/david-andrew/escort_mission_2020/releases/download/0.5.0/linux_0_5_0.zip">
-                        Silver Void 0.1.7 (linux)
+                    <Link href="https://github.com/david-andrew/SilverVoid/releases/download/0.1.7/linux_0.1.7.zip">
+                        Download linux_0.1.7
                     </Link>
                 </IconBullet>
                 <IconBullet icon="windows">
-                    <Link href="https://github.com/david-andrew/escort_mission_2020/releases/download/0.5.0/windows_0_5_0.zip">
-                        Silver Void 0.1.7 (windows)
+                    <Link href="https://github.com/david-andrew/SilverVoid/releases/download/0.1.7/windows_0.1.7.zip">
+                        Download windows_0.1.7
                     </Link>
                 </IconBullet>
                 <IconBullet icon="apple">
-                    <Link href="https://github.com/david-andrew/escort_mission_2020/releases/download/0.5.0/mac_0_5_0.zip">
-                        Silver Void 0.1.7 (mac)
+                    <Link href="https://github.com/david-andrew/SilverVoid/releases/download/0.1.7/mac_0.1.7.zip">
+                        Download mac_0.1.7
                     </Link>
                 </IconBullet>
-            </IconBulletList> */}
-
+                {/* </IconBulletList>
             <br />
-            <H3>Links</H3>
-            <IconBulletList>
-                <IconBullet icon="gamepad">
-                    <Link href="https://itch.io/jam/acerola-jam-0/rate/2557875">Jam Submission Page</Link>
-                </IconBullet>
-                <IconBullet icon="gamepad">
-                    <Link href="https://dsamson.itch.io/silver-void">Itch.io Game Page</Link>
-                </IconBullet>
+            <H3>Source</H3>
+            <IconBulletList> */}
                 <IconBullet icon="github">
-                    <Link href="https://github.com/david-andrew/SilverVoid">Github Repo</Link>
+                    <Link href="https://github.com/david-andrew/SilverVoid">Source Code</Link>
                 </IconBullet>
             </IconBulletList>
         </>
