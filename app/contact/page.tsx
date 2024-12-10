64ba79f14d7af941f925d2a5ba031861d9fef8da
David Samson <david.andrew.engineer@gmail.com>
2024-12-10 01:13:54 -0500

    send accent color to contact 3d scene iframe

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,12 +1,51 @@
+"use client";
 import { IconBullet, IconBulletList } from '../(components)/icon_bullet'
 import { NavbarDummy } from '../(components)/navbar'
 import { Link } from '../(components)/ui'
+import { useState, useEffect, useRef } from 'react'
+
+// convert a hex color e.g. "#002d72" to an rgb tuple e.g. [0, 0.176470588, 0.447058824]
+const hex_to_rgb = (hex: string): [number, number, number] => {
+    const r = parseInt(hex.slice(1, 3), 16) / 255
+    const g = parseInt(hex.slice(3, 5), 16) / 255
+    const b = parseInt(hex.slice(5, 7), 16) / 255
+    return [r, g, b]
+}
 
 const Page = () => {
+    // keep track of/watch for changes of the accent color in document.documentElement.style.getPropertyValue('--accent-color')
+    const [accentColor, setAccentColor] = useState<string>()
+    const iframeRef = useRef<HTMLIFrameElement>(null)
+
+    // watch for changes to the accent color
+    useEffect(() => {
+        const updateAccentColor = () => {
+            // console.log('updateAccentColor', document.documentElement.style.getPropertyValue('--accent-color'))
+            setAccentColor(document.documentElement.style.getPropertyValue('--accent-color'))
+        }
+        updateAccentColor()
+
+        // use mutation observer to watch for changes to the accent color
+        const observer = new MutationObserver(updateAccentColor)
+        observer.observe(document.documentElement, { attributes: true })
+        return () => observer.disconnect()
+    }, [])
+
+    // post the accent color to the iframe
+    useEffect(() => {
+        if (iframeRef.current?.contentWindow && accentColor) {
+            iframeRef.current.contentWindow.postMessage({type: 'SET_COLOR', color: hex_to_rgb(accentColor)}, '*')
+        }
+    }, [accentColor])
+
     return (
         <>
             <div className="flex flex-col h-full">
-                <iframe src="https://david-andrew.github.io/BusinessCard/" className="flex-grow" />
+                <iframe 
+                    src="https://david-andrew.github.io/BusinessCard/" 
+                    className="absolute inset-0 w-full h-full"
+                    ref={iframeRef}
+                 />
                 {/* <div className="w-full flex flex-row items-center justify-center flex-grow">
                     <div className="outline outline-white py-16 px-10 md:py-20 md:px-10 lg:py-36 lg:px-20">
                         <IconBulletList>
@@ -27,7 +66,7 @@ const Page = () => {
                         </IconBulletList>
                     </div>
                 </div> */}
-                {/* <NavbarDummy /> */}
+                <NavbarDummy />
             </div>
         </>
     )
